5bbd6a5af34d9aa2e0b0a6487fba6679dddba4d0
josue <83614619+josuefeo123@users.noreply.github.com>
2021-05-05 21:06:39 -0500

    js
    js subiendo

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,55 @@
+alert("Hola Mundo")
+
+const nombre = prompt('Cual es tu nombre?');
+document.querySelector('.contenido').innerHTML = ` ${nombre} está aprendiendo JavaScript Moderno`;
+
+
+
+const hola = "Hola desde la consola";
+console.log(hola);
+
+
+console.log([1, 2, 3, 4]);
+
+
+let obj = { nombre: "juan", profesion: "Desarrollador Web" }
+console.log( obj );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.table([1, 2, 3, 4]);
+
+
+console.error("Algo salió mal");
+
+
+console.clear();
+
+
+console.warn("Eso no esta permitido");
+
+
+console.time('Hola'); console.warn("Eso no esta permitido"); console.warn("Eso no esta permitido");
+console.warn("Eso no esta permitido");
+console.timeEnd('Hola');
+   
+
+
+console.log('Hola')
+console.log('Mundo')
+
+
+console.log('Hola'); console.log('Mundo');
+
+
